b9a8e8616ee8d669211b24caf77cae3cf8bd6afc
Timothy Sukamtoh <timtoh168@gmail.com>
2023-04-20 16:19:26 -0700

    complete CR1 and start using sessions

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-RESPONSES = []
 
 @app.get("/")
 def home_page():
@@ -20,34 +19,37 @@ def home_page():
 def start_survey():
     """Starts the given survey"""
 
+    session[“responses”] = []
+
     return redirect("/questions/0")
 
 @app.get("/questions/<int:question_number>")
 def question_page(question_number):
     """Loads the page for the current question"""
 
-    current_question = survey.questions[question_number]
+    if question_number > len(survey.questions):
+        return redirect(f"/questions/{len(session[“responses”])}")
 
-    return render_template("question.html", question = current_question, question_number=question_number)
+    question = survey.questions[question_number]
 
-@app.post("/answer/<int:question_number>")
-def submit_answer(question_number):
-    #  """checks for answer, if answer, continue to next question"""
-    answer = request.form.get("answer")
+    return render_template("question.html", question = question, question_number=question_number)
 
+@app.post("/answer")
+def submit_answer():
+    """checks for answer, if answer, continue to next question"""
+
+    answer = request.form["answer"]
     if answer:
-        RESPONSES.append(answer)
-        question_number += 1
-        if question_number == len(survey.questions):
+        session[“responses”].append(answer)
+        if len(session[“responses”]) >= len(survey.questions):
             return redirect("/completion")
-        else:
-            return redirect(f"/questions/{question_number}")
-    else:
-        return redirect(f"/questions/{question_number}")
+    return redirect(f"/questions/{len(session[“responses”])}")
 
 @app.get("/completion")
 def completion():
-    return render_template("completion.html", responses=RESPONSES, questions=survey.questions)
+    """Sends user to survery completion page"""
+
+    return render_template("completion.html", responses=session[“responses”], questions=survey.questions)
 
 
 
