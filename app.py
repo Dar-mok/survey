46ca9ef83203954be4f8f8cb7a99f0308ca7368b
Russell Jones <justyrones@gmail.com>
2023-04-20 12:49:35 -0600

    step 2 done

--- app.py
+++ app.py
@@ -6,4 +6,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
+RESPONSES = []
+
+@app.get("/")
+def home_page():
+    return render_template("survey_start.html",title=survey.title, instructions=survey.instructions)
+
+@app.get("/begin")
+def questions_link():
+    return redirect("/questions/0")
+
+
 debug = DebugToolbarExtension(app)
